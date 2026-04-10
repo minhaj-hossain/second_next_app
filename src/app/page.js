b3269be81e3e4af2c4dca1d5ba6b582c2eb2bbd5
Minhaj-Hossain <minhaj1080@gmail.com>
2026-04-10 17:24:52 +0600

    differentiate between use client and server

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react";
 
 export default function Home() {
+
+
+  const [counter, setCounter] = useState(0)
   return (
     <div>
-
       Hello there homepage!
 
+      <h3>{counter}</h3>
+      <div
+        onClick={() => setCounter(counter + 1)}
+        className="bg-red-400 py-3.5 px-2.5 text-white cursor-pointer">button</div>
     </div>
   );
 }
